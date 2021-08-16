6c473c03b35feb6d5f75f36e863995010f84d717
alaminpro <mdalamin6554@gmail.com>
2021-08-16 20:34:51 +0600

    Initial Stage

--- resources/views/frontend/includes/ads-image.blade.php
+++ resources/views/frontend/includes/ads-image.blade.php
@@ -2,7 +2,7 @@
     <h2 class="pb-3 text-xs text-yellow-500">Advertise with us!</h2>
     <div class="md:w-10/12 xl:w-9/12">
         @forelse($ads as $ad)
-        <a href="{{ $ad->link }}" class="block mb-2">
+        <a href="{{ $ad->link }}" class="block mb-2" target="_blank">
             <img class="sm:hidden w-full" src="{{ $ad->mobile_ads }}" alt="{{ $ad->name }}">
             <img class="hidden sm:block w-full" src="{{ $ad->desktop_ads }}" alt="{{ $ad->name }}">
         </a>
